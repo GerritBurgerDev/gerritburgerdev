3e2ed79bb9b53c9256ec9cfbb63d9ab0454bd9ef
Gerrit Burger <21261687@sun.ac.za>
2020-09-11 00:03:39 +0200

    UPDATE: 1) Created the skeleton 2) Finished the about me sheet 3) Finished the dynamic experience cards 4) Finished the dynamic education cards

---
+++ src/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-footer
+      fixed
+      class="font-weight-medium"
+      elevation="4"
+  >
+    <v-col
+        class="text-center"
+        cols="12"
+    >
+      <v-btn icon href="https://github.com/GerritBurgerDev/" target="_blank">
+        <v-icon medium>mdi-github</v-icon>
+      </v-btn>
+      <v-btn icon href="https://www.linkedin.com/in/gerritburgerdev/" target="_blank">
+        <v-icon medium>mdi-linkedin</v-icon>
+      </v-btn>
+      <v-btn icon href="#" target="_blank">
+        <v-icon medium>mdi-email</v-icon>
+      </v-btn>
+      <v-btn icon href="#" target="_blank">
+        <v-icon medium>mdi-phone</v-icon>
+      </v-btn>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+
+  data() {
+    return {}
+  },
+};
+</script>
